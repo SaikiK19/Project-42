1c4cf49e552c0310b102c44689bc77e2cb13f4c6
SaikiK19 <75398184+SaikiK19@users.noreply.github.com>
2021-07-22 18:14:09 -0400

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -8,27 +8,28 @@ var c,d;
 
 
 function setup() {
-  createCanvas(800,400);
+  createCanvas(800,800);
   angleMode(DEGREES)
-  hr = hour()
-  mn = minute()
-  sc = second()
 }
 
 function draw() {
-  background(255,255,255);
+  background(0);
   console.log(mn)
   //scAngle = map(sc,0,60,0,360);
   //translate(p5.Vector.fromAngle(millis() / 1000, 60));
   //d = map(mouseX, 0, width, 40, 300);
-  
+  hr = hour()
+  mn = minute()
+  sc = second()
+
+
   push();
   scAngle = map(sc, 0, 100, 0, 360);
   translate(400,200);
   rotate(scAngle);
   stroke(255,0,0);
   strokeWeight(7);
-  line(0,0,100,0);
+  line(0,0,350,0);
   pop();
   push()
   hrAngle = map(hr, 0, 100, 0, 360);
@@ -44,7 +45,24 @@ function draw() {
   stroke(0,255,0);
   strokeWeight(7);
   line(0,0,250,0);
-
+  push()
+  strokeWeight(10)
+  stroke(0,255,0)
+  noFill()
+  arc(0,0,500,500,0,mnAngle)
+  pop()
+  push()
+  strokeWeight(10)
+  stroke(0,0,255)
+  noFill()
+  arc(0,0,300,300,0,hrAngle)
+  pop()
+  push();
+  strokeWeight(10)
+  stroke(255,0,0)
+  noFill()
+  arc(0,0,700,700,0,scAngle)
+  pop()
   //ellipse(600, 300, 100, 500);
   drawSprites();
 }
